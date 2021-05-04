ea12d72bed4aadbc9ffa37e66497b85b30fe25d5
Guzzler <sharangpai123@gmail.com>
2021-05-05 04:24:14 +0530

    complete email verification

--- src/react-app/components/MainRouter.js
+++ src/react-app/components/MainRouter.js
@@ -8,6 +8,7 @@ import {
 import PageNotFound from './common/error-pages/PageNotFound'
 import Landing from './Landing'
 import Unsubscribe from './Unsubscribe'
+import VerifyEmail from './VerifyEmail'
 
 
 function MainRouter () {
@@ -17,6 +18,7 @@ function MainRouter () {
         <Row className='overflow-auto display-block' style={{height: '100vh'}}>
           <Switch location={location}>
             <Route path="/unsubscribe" component={Unsubscribe} key="unsubscribe" />
+            <Route path="/verify_email" component={VerifyEmail} key='verify_email' />
             <Route path="/" component={Landing} key="landing" />
             <Route render={() => <PageNotFound />} key="notFound" />
           </Switch>
--- src/react-app/components/VerifyEmail.js
+++ src/react-app/components/VerifyEmail.js
@@ -2,28 +2,27 @@ import React, { useEffect } from 'react'
 import {  Row, Card } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import {
-    unsubscribeEmail
+    onEmailVerify
 } from '../actions/index'
 import Loader from '../components/common/Loader'
 import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons'; 
 import { useLocation } from 'react-router'
 
 
-
-const Unsubscribe = () =>  {
+const VerifyEmail = () =>  {
   const dispatch = useDispatch()
   const search = useLocation().search
   useEffect(() => {
     const email = new URLSearchParams(search).get('email');
-    const subscriptionId = new URLSearchParams(search).get('subscription_id');
-    dispatch(unsubscribeEmail(email,subscriptionId))
-  },[search, dispatch]);
-  const unsubscribe = useSelector((state) => state.base.unsubscribe);
+    const token = new URLSearchParams(search).get('token');
+    dispatch(onEmailVerify(email, token))
+  }, [search, dispatch]);
+  const verifyEmail = useSelector((state) => state.base.verifyEmail);
   const {
       isDone,
       success,
       email,
-  } = unsubscribe;
+  } = verifyEmail;
   return (
     <Row className={'padding--sides width-100 height-100'}>
       <Card className='border-round padding--sides padding--ends margin--ends background-grey center' style={{width: '100%'}}>
@@ -33,7 +32,7 @@ const Unsubscribe = () =>  {
           <>
             <CheckCircleOutlined className='f72 text-green' />
             <div className='text-black center margin--top f18'>
-              You have successfully unsubscribed! Your email {email} will no longer receive notifications . 
+              You have successfully subscribed! Your email-{email} will now receive notifications about slot preferences as they become available. 
             </div>
           </> :
           <>
@@ -49,4 +48,4 @@ const Unsubscribe = () =>  {
   )
 }
 
-export default Unsubscribe
+export default VerifyEmail
