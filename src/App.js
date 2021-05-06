9f3c41b72bb0bfd7efe81a5aa31170f37323f5e6
Guzzler <sharangpai123@gmail.com>
2021-05-07 02:49:02 +0530

    hotfix/app-routing

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ import {
   Switch
 } from 'react-router-dom'
 import Base from './react-app/components/Base'
-import PageNotFound from './react-app/components/common/error-pages/PageNotFound'
-
 
 const App = () => (
   <Router>
     <div>
       <Switch>
         <Route path="/" component={Base} key='base'/>
-        <Route render={() => <PageNotFound />} key="notFound" />
       </Switch>
     </div>
   </Router>
