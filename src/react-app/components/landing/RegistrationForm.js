ea41dc97ad7f89891e38bf6c309b657c3e38a73e
Abizer Lokhandwala <abizer.lokhandwala@codenation.co.in>
2021-05-07 21:38:00 +0530

    add : to email and phnumber, replace how it works? with :

--- src/react-app/components/landing/RegistrationForm.js
+++ src/react-app/components/landing/RegistrationForm.js
@@ -51,10 +51,10 @@ const RegistrationForm = (props) => {
           /> :
           <>
             <div className='para-style left margin--bottom'>Choose your preferences and get vaccine availability sent straight to your mailbox!</div>
-            <div className='label'>Email</div>
+            <div className='label'>Email:</div>
             <ErrorMessage message={errors.email} />
             <Input block='true' value={email} onChange={(e) => onChangeRegistrationField({'email': e.target.value})} />
-            <div className='label'>Phone Number (optional) </div>
+            <div className='label'>Phone Number (optional):</div>
             <ErrorMessage message={errors.phoneNumber} />
             <Input block='true' value={phoneNumber} onChange={(e) => onChangeRegistrationField({'phoneNumber': e.target.value})}/>
             <div className='label'>Chosen Districts: </div>
