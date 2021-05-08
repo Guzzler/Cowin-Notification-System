faee8ff74c951ecf077120d432e0487815f7fba7
Arpit Mathur <arpitmathur19@gmail.com>
2021-05-08 04:49:33 -0400

    Remove Unused dependencies

--- src/react-app/components/Base.js
+++ src/react-app/components/Base.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Affix } from 'antd'
+import { Layout } from 'antd'
 
 import MainRouter from './MainRouter'
 import PageFooter from './PageFooter';
--- src/react-app/components/PageFooter.js
+++ src/react-app/components/PageFooter.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 import { HeartFilled } from '@ant-design/icons'; 
 
 const PageFooter = () => {
