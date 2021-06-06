33a59979d5978aadea1f707df735a3f135234377
Sharang Pai <Guzzler@users.noreply.github.com>
2021-06-07 00:39:22 +0530

    Fix sputnik Overflow Issue

--- src/react-app/components/landing/registration/SubscriptionCard.js
+++ src/react-app/components/landing/registration/SubscriptionCard.js
@@ -67,10 +67,10 @@ const SubscriptionCard = (props) =>  {
       </Row>
       <div className='label'> Vaccination Type: </div>
       <Radio.Group className='width-100' value={vaccine} onChange={(e) => props.onChangeSubscriptionField({'vaccine': e.target.value })}>
-          <Radio.Button style={{width: '22.5%' }} className='f10 margin-half--right center' value="covaxin">Covaxin</Radio.Button>
-          <Radio.Button style={{width: '22.5%' }} className='f10 margin-half--right center' value="covishield">Covishield</Radio.Button>
-          <Radio.Button style={{width: '22.5%' }} className='f10 margin-half--right center' value="sputnik v">Sputnik V</Radio.Button>
-          <Radio.Button style={{width: '22.5%' }} className='f10 center' value="both">All</Radio.Button>
+          <Radio.Button style={{width: '22%' }} className='f10 margin-half--right center' value="covaxin">Covaxin</Radio.Button>
+          <Radio.Button style={{width: '22%' }} className='f10 margin-half--right center' value="covishield">Covishield</Radio.Button>
+          <Radio.Button style={{width: '22%' }} className='f10 margin-half--right center' value="sputnik v">SputnikV</Radio.Button>
+          <Radio.Button style={{width: '22%' }} className='f10 center' value="both">All</Radio.Button>
         </Radio.Group>
       <div className='label'> Age Group: </div>
       <Radio.Group className='width-100' value={ageGroup} onChange={(e) => props.onChangeSubscriptionField({'ageGroup': e.target.value })}>
