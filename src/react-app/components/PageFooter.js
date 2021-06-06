0fbcff98bfd2a2db4bda154722d144f7464de052
Sharang Pai <Guzzler@users.noreply.github.com>
2021-06-06 20:32:06 +0530

    Add Names again

--- src/react-app/components/PageFooter.js
+++ src/react-app/components/PageFooter.js
@@ -6,6 +6,14 @@ const PageFooter = () => {
     <div>
     <div >Made with <HeartFilled /></div>
     <div>
+        By - 
+        <a href='https://github.com/abizerlokhandwala' target="_blank" rel="noopener noreferrer"> Abizer Lokhandwala</a>,
+        <a href='https://github.com/shloksingh10' target="_blank" rel="noopener noreferrer"> Shlok Singh</a>,
+        <a href='https://github.com/mrmathur' target="_blank" rel="noopener noreferrer"> Arpit Mathur</a>,
+        <a href='https://github.com/Guzzler' target="_blank" rel="noopener noreferrer"> Sharang Pai</a>,
+        <a href='https://www.linkedin.com/in/pujan-parikh-818234165/' target="_blank" rel="noopener noreferrer"> Pujan Parikh</a>
+    </div> 
+    <div>
         Contribute here -
         <a href='https://github.com/Guzzler/Cowin-Notification-System' target="_blank" rel="noopener noreferrer"> Frontend</a>,
         <a href='https://github.com/abizerlokhandwala/Cowin-Notification-Service' target="_blank" rel="noopener noreferrer"> Backend</a>,
