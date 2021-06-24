935af0da3c7659284099813e0da45ff6cd2963b9
Abizer Lokhandwala <abizer.lokhandwala@codenation.co.in>
2021-06-24 22:31:26 +0530

    change successful subscription msg

--- src/react-app/components/landing/registration/SuccessfulRegistration.js
+++ src/react-app/components/landing/registration/SuccessfulRegistration.js
@@ -8,7 +8,7 @@ const SuccessfulRegistration = (props) => {
 	<div className='center'>
 		<CheckCircleOutlined className='f72 text-green' />
 		<div className='para-style center margin--top '>
-			You have successfully registered! We have sent a verification email. Once verified, you will receive information about any new slots that open up according to the preferences you had set. 
+			You have successfully registered!<br/>If this is your first subscription, you will receive a verification email.<br/>Once verified, you will receive information about any new slots that open up according to the preferences you had set.
 		</div>
 		<Button block='true' className='submit-button margin--ends text-white' onClick={() => props.resetRegisterForm()}> Subscribe Again</Button>
 	</div>
